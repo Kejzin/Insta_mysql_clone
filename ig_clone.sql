316136ea84717691ac8af5e18395d04c3247f40f
Kejzin <kejzin@gmail.com>
2020-12-29 19:44:09 +0100

    Add creation of 'likes' table

--- ig_clone.sql
+++ ig_clone.sql
@@ -18,9 +18,18 @@ CREATE TABLE photos(
 CREATE TABLE comments(
 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	comment_text VARCHAR(255) NOT NULL,
-	user_id INTEGER,
-	photo_id INTEGER,
+	user_id INTEGER NOT NULL,
+	photo_id INTEGER NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW(),
 	FOREIGN KEY(user_id) REFERENCES users(id),
 	FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+CREATE TABLE likes(
+	user_id INTEGER NOT NULL,
+	photo_id INTEGER NOT NULL,
+	created_at TIMESTAMP DEFAULT NOW(),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(photo_id) REFERENCES photos(id),
+	PRIMARY KEY(user_id, photo_id)
 );
