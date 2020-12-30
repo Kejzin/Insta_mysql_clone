b5f80943b1809ae3385f31e83c06b1a5c41424dc
Kejzin <kejzin@gmail.com>
2020-12-30 13:22:35 +0100

    Challenge 3 solution comments

--- challenge3.sql
+++ challenge3.sql
@@ -1,10 +1,12 @@
 /* 3. We want ot target our inactive users with an email campaign. Find the users who have never posted a photo */
 
 SELECT 
-	username,
-	COUNT(photos.id) AS num_user_photos
+	username
+	-- COUNT(photos.id) AS num_user_photos
 FROM users
 	LEFT JOIN photos 
 		ON users.id = photos.user_id
 WHERE photos.id IS NULL
-GROUP BY users.id;
+-- GROUP BY users.id;
+
+/* If I remove that "COUNT" there is no need for GROUP BY clausule */
