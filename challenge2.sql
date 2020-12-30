02d9c2ac61e1b3eda550d83b83c8ec521624b433
Kejzin <kejzin@gmail.com>
2020-12-30 12:52:50 +0100

    My solution for challenge 2

--- challenge2.sql
+++ challenge2.sql
@@ -1,1 +1,10 @@
 /* 2. What day of the week do most users regiester on? (Can be important for scheduling an ad campgain) */
+
+SELECT 
+	DAYNAME(created_at) AS Day,
+	COUNT(*)	    AS registrations
+FROM users
+	GROUP BY DAYOFWEEK(created_at)
+ORDER BY registrations DESC
+LIMIT 1;
+
