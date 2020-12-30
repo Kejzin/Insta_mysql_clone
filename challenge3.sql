6ed078bbc195a3ee53ae977f25c0efe28830a196
Kejzin <kejzin@gmail.com>
2020-12-30 13:07:55 +0100

    [Part] Challenge 2 way to solution

--- challenge3.sql
+++ challenge3.sql
@@ -1,1 +1,8 @@
 /* 3. We want ot target our inactive users with an email campaign. Find the users who have never posted a photo */
+
+SELECT 
+	username 
+FROM users
+	LEFT JOIN photos 
+		ON photos.user_id = users.id
+GROUP BY users.id;
