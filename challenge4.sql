0fb65ec0998ef6d0695ec87023c5d97469846bed
Kejzin <kejzin@gmail.com>
2020-12-30 20:27:00 +0100

    My solution for challenge4

--- challenge4.sql
+++ challenge4.sql
@@ -1,1 +1,12 @@
 /* 4. We're running a new contest to see who can get the most likes on a single photo. Who won??!! */
+
+SELECT 
+	photos.id AS photo_id,
+	photos.image_url,
+	COUNT(*) AS num_likes
+FROM photos
+	LEFT JOIN likes
+		ON photos.id = likes.photo_id
+GROUP BY photos.id
+ORDER BY num_likes DESC
+LIMIT 1;
