5d2adbce9d25955a97063d1da57051d934c5377e
Kejzin <kejzin@gmail.com>
2020-12-30 13:02:41 +0100

    Comment to challenge 2 solutions

--- challenge2.sql
+++ challenge2.sql
@@ -8,3 +8,5 @@ FROM users
 ORDER BY registrations DESC
 LIMIT 1;
 
+/* It is as in video BUT there are 2 days that has 16 registrations. You can Limit it to 1 or 2, but if you limit it to 1 you will miss "true" answer. Maybe thee is a solution to make some "Find how many days has the same amount as that LIMIT 1 day and display all that has the same amount of registrations" but ismay be to advanced at the moment" */
+
