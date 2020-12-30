9539319b9b0a8729026b9683ea7e622be5e3bdc8
Kejzin <kejzin@gmail.com>
2020-12-30 20:50:14 +0100

    Challenge 5 solution, watched on video I couldn't figure it out!

--- challenge5.sql
+++ challenge5.sql
@@ -1,1 +1,4 @@
 /* Our investors want to know... How many times does the average user post (photos)? */
+
+SELECT 
+	(SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users); -- yeah that subqueries
